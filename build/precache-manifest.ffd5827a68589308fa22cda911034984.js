286e42923b6e4bfd8bb334d45ccc746fdeb954c7
MBL <imamosi50@gmail.com>
2020-09-21 09:50:17 +0300

    first commit

---
+++ build/precache-manifest.ffd5827a68589308fa22cda911034984.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a315cbc2c4fb9516985354733455ef5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ca5411f3cb874653a63d",
+    "url": "/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "fa29ee0438522e5dd87b",
+    "url": "/static/css/main.93bae99b.chunk.css"
+  },
+  {
+    "revision": "ca5411f3cb874653a63d",
+    "url": "/static/js/2.8bf57d0e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.8bf57d0e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa29ee0438522e5dd87b",
+    "url": "/static/js/main.22fa51c3.chunk.js"
+  },
+  {
+    "revision": "af1ee15f3b8733767315",
+    "url": "/static/js/runtime-main.8a8ac9cd.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
